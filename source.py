8d3dc490af6949e735f155177a0278d1f7e67716
Austin LaShure <austinglashure@gmail.com>
2021-06-19 22:34:29 -0400

    created content object and made soup object with it, cleaned up some code from previous commit

--- source.py
+++ source.py
@@ -7,14 +7,22 @@ import bs4
 print("Requests: {}".format(requests.__version__))
 print("Beautiful Soup: {}".format(bs4.__version__))
 
-# verifies that URL is valid
+# creates requests object and verifies link is valid
 url = "https://www.google.com"  # will eventually be page with requisite weather data
 result = requests.get(url)  # creates requests object
 print("URL: {}".format(url))
+
 url_status = result.status_code  # creates status code variable
 print("Status Code: {}".format(url_status))
-if url_status == 200:
+if url_status >= 200 and url_status < 300:
+    # successful status codes for http begin with 2--
     print("Link is valid")
 else:
     print("Error with the link")
+
+# create web source variable
+source = result.content
+
+# creates soup object from web source
+soup = bs4.BeautifulSoup(source, 'lxml')
 
