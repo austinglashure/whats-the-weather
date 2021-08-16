86458078f660ba12d1862376a6143b2a33dfa0ee
Austin LaShure <austinglashure@gmail.com>
2021-08-16 04:13:28 -0400

    scrapes temp, summary, and precipitation chance

--- source.py
+++ source.py
@@ -1,5 +1,5 @@
-
 # imported scraping modules
+from googlesearch import search
 import requests
 import bs4
 
@@ -7,8 +7,33 @@ import bs4
 print("Requests: {}".format(requests.__version__))
 print("Beautiful Soup: {}".format(bs4.__version__))
 
-# creates requests object and verifies link is valid
-url = "https://orange.weatherstem.com/ucf"  # will page with requisite weather data
+# get the link
+search_text = "weather.com today oviedo fl"
+links_list = search(search_text, stop=15)
+link = ''
+for foo in links_list:
+    if 'today' in foo:
+        link = foo
+        break
+
+# make scrape objects
+req = requests.get(link)
+soup = bs4.BeautifulSoup(req.content, 'lxml')
+
+# parse relevant info
+foo = soup.find('div', class_='CurrentConditions--primary--2SVPh')
+fub = foo.text
+parts = fub.split('°')
+# said relevant info
+temperature = parts[0]
+summary = parts[1]
+# get rain chance
+precip_temp = soup.find('div', class_='CurrentConditions--precipValue--3nxCj').text
+aye = precip_temp.index('%')
+precipitation_chance = precip_temp[0, aye+1]
+print(precipitation_chance)
+
+'''
 result = requests.get(url)  # creates requests object
 print("URL: {}".format(url))
 
@@ -20,8 +45,10 @@ if url_status >= 200 and url_status < 300:
 else:
     print("Error with the link")
 
-# create web source variable
-source = result.content
-
 # creates soup object from web source
-soup = bs4.BeautifulSoup(source, 'html.parser')
+soup = bs4.BeautifulSoup(result.content, 'lxml')
+dd_tags = soup.find_all('dd')
+for tag in dd_tags:
+    print(tag)
+'''
+ 
